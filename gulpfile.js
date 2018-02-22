376d3025a92e4a9c5ec689873010775e7c2e4356
sergeirepetyuk <sergeirepetyuk@gmail.com>
2018-02-22 20:44:05 +0200

    add browser sync to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,47 @@
 var gulp = require('gulp');
 var fs = require("fs");
 var {promisify} = require("util");
+const browserSync = require('browser-sync').create();
 const readFile = promisify(fs.readFile);
-var browserSync = require('browser-sync').create();
-
-gulp.task('default', ['less', "mustache","copy","server"], function () {
-    gulp.watch('./src/style/**/*.less', ['less']);
-    gulp.watch('./src/img/*.*', ['copy']);
-    gulp.watch([
-        './src/data/data.json',
-        './src/**/*.html'
-    ], ['mustache']);
-});
-
 
 var less = require('gulp-less');
 var path = require('path');
 
 gulp.task('less', function () {
-    return gulp.src('./src/style/**/*.less')
-        .pipe(less({
-            paths: [path.join(__dirname, 'less', 'includes')]
-        }))
-        .pipe(gulp.dest('./docs/style'));
+  return gulp.src('./src/style/**/*.less')
+  .pipe(less({
+    paths: [ path.join(__dirname, 'less', 'includes') ]
+  }))
+  .pipe(gulp.dest('./docs/style'))
+  .pipe(browserSync.stream());
 });
 
 var mustache = require("gulp-mustache");
 gulp.task("mustache", async () => {
-    const data = JSON.parse(await readFile("./src/data/data.json", "utf-8"));
+  const data = JSON.parse(await readFile("./src/data/data.json", "utf-8"));
 
-    return gulp.src("./src/**/*.html")
-        .pipe(mustache(data))
-        .pipe(gulp.dest("./docs"));
+  return gulp.src("./src/**/*.html")
+  .pipe(mustache(data))
+  .pipe(gulp.dest("./docs"));
 })
 
 gulp.task('copy', function () {
-    gulp.src("./src/img/*.*")
-        .pipe(gulp.dest('./docs/img/'));
+    gulp.src('./src/images/*.*')
+        .pipe(gulp.dest('./docs/images/'));
 });
 
 
-gulp.task('server', function(){
+gulp.task('default', ["less", "mustache"], function () {
     browserSync.init({
-        server:{
-            baseDir: "docs/"
-        },
-        files: ['docs/*.html','docs/*.css','docs/img/*.*']
+        server: "./docs"
     });
+
+    gulp.watch('./src/style/**/*.less', ['less']);
+    gulp.watch("./src/images/*.*", ["copy"]);
+
+    gulp.watch([
+        './src/data/data.json',
+        './src/**/*.html'
+    ], ['mustache'])
+    .on('change', browserSync.reload);
 });
